30c4ef7acf0987ca7f26da7d2f6e9c06c82dfae4
abhi178134 <abhiijeet178134@gmail.com>
2021-07-15 16:22:39 +0530

    1.6 Done

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,22 +1,35 @@
 import React, {useState} from 'react'
 
-const App = () => {
-  const [ counter, setCounter ] = useState(0)
 
-  const increaseByOne =() => setCounter(counter+1)
-  const decrementByOne =() => setCounter(counter-1)
-  const setToZero =() => setCounter(0)
-  
-  const Display = ({ counter }) => <div>{counter}</div>
-  
-  const Button = ({ handleClick, text }) => (
-    <button onClick={handleClick}>
-      {text}
-    </button>
-  )
+
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>
+    {text}
+  </button>
+)
+
+const App = () => {
+
+  const [good,setGood]=useState(0)
+  const [neutral,setNeutral]=useState(0)
+  const[bad,setBad]=useState(0)
+
+  const handleGood = () => setGood(good+1)
+  const handleNeutral =() => setNeutral(neutral+1)
+  const handleBad =() => setBad(bad+1)
 
   return (
     <div>
+      <h1>give feedback</h1>
+      <Button handleClick={handleGood} text = 'Good'/>
+      <Button handleClick={handleNeutral} text='Neutral' />
+      <Button handleClick={handleBad} text='Bad' />
+      <h2>statistics</h2>
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      
+{/* 
       <Display counter ={counter}/>
       <Button 
         handleClick={increaseByOne}
@@ -29,7 +42,7 @@ const App = () => {
       <Button
         handleClick={decrementByOne}
         text='Minus'
-      />
+      /> */}
       </div>
     
   )
